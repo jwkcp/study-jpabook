4bdddfb41aaf46b434abc7121344cb76848fe339
jwkcp <jaewoong.go@gmail.com>
2023-03-05 23:44:55 +0900

    프로젝트 환경설정 완료

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+//    @Rollback(value = false)
+    public void save() {
+        Member member = new Member();
+        member.setUsername("kim1");
+
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.findById(savedId);
+
+        Assertions.assertThat(member.getId()).isEqualTo(findMember.getId());
+        Assertions.assertThat(member.getUsername()).isEqualTo(findMember.getUsername());
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+    @Test
+    void findById() {
+    }
+}
