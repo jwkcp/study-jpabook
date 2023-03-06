c865b17055903444c5babcf551ba613ab7de44c3
jwkcp <jaewoong.go@gmail.com>
2023-03-07 08:54:05 +0900

    엔티티 클래스 개발1 - 도중

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@Table(name = "orders")
+public class Order {
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+}
