2942fe7df9349263c4524626326ea68d85527786
jwkcp <jaewoong.go@gmail.com>
2023-03-15 08:36:51 +0900

    엔티티 클래스 개발2 - 완료

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,18 +17,19 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order_id")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
-    private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
 }
