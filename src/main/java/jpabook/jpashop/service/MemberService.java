34d3bb93e04f2d992c7c5176e2495e4fc73b2cba
jwkcp <jaewoong.go@gmail.com>
2023-03-19 23:43:29 +0900

    회원 서비스 개발 완료

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -4,15 +4,18 @@ import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class MemberService {
     @Autowired
     private MemberRepository memberRepository;
     
     // 회원 가입
+    @Transactional
     public Long join(Member member) {
         validateDuplicateMember(member);
         memberRepository.save(member);
