30388ddee8dd5675e7a649c8305ba2f58ae2ba32
jwkcp <jaewoong.go@gmail.com>
2023-03-21 08:18:07 +0900

    회원 기능 테스트 완료

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -43,7 +43,7 @@ class MemberServiceTest {
         Assertions.assertEquals(member, findMember);
     }
 
-    @Test(expected) // TODO: JUnit5 문법으로 바꿔야 함
+    @Test // TODO: JUnit5 문법으로 바꿔야 함
     public void duplicatedMember() throws Exception {
         Member member1 = new Member();
         Member member2 = new Member();
@@ -51,14 +51,13 @@ class MemberServiceTest {
         member1.setName("kim1");
         member2.setName("kim1");
 
+        // 첫번째 회원 가입
+        memberService.join(member1);
 
-        try {
-            Long savedMember1 = memberService.join(member1);
-            Long savedMember2 = memberService.join(member2);
-        } catch (IllegalStateException e) {
-            return;
-        }
+        // 중복 예외가 발생하는 부분
+        Assertions.assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
-        Assertions.fail("예외가 발생해야 합니다.");
+        // 위에서 예외 발생 여부를 검증하므로 삭제
+        // Assertions.fail("예외가 발생해야 합니다.");
     }
 }
