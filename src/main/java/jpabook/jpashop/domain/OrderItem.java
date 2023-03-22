69fd30b755af67aa8d0be24a71f2eb35a2decae8
jwkcp <jaewoong.go@gmail.com>
2023-03-23 08:55:45 +0900

    주문, 주문상품 엔티티 개발 완료

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -23,4 +23,24 @@ public class OrderItem {
     private int orderPrice;
 
     private int count;
+
+    //==생성 메서드==//
+    public OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        item.addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * count;
+    }
 }
